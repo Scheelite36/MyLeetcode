170b5a0be790517adfeed1b495becb188beeed44
scheelite <jwei.gan@qq.com>
2022-01-16 23:57:45 +0800

    binary tree search

---
+++ 1361.验证二叉树.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1361 lang=java
+ *
+ * [1361] 验证二叉树
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+
+    }
+}
+// @lc code=end
+
--- 98.验证二叉搜索树.java
+++ 98.验证二叉搜索树.java
@@ -22,22 +22,21 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if(root == null){
+        return isValidBST(root,Long.MAX_VALUE,Long.MIN_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long maxVal, long minVal){
+        if (root == null){
+            // 每个节点如果超过这个范围，直接返回false
+            return true;
+        }
+        if ((root.val >= maxVal) || (root.val <= minVal)){
             return false;
         }
-        if(root.left != null){
-            if(!(root.left.val < root.val)){
-                return false;
-            }
-            isValidBST(root.left);
-        } 
-        if(root.right != null){
-            if(!(root.right.val>root.val)){
-                return false;
-            }
-            isValidBST(root.right);
-        }     
-        return true;
+        //这里再分别以左右两个子节点分别判断，
+        //左子树范围的最小值是minVal，最大值是当前节点的值，也就是root的值，因为左子树的值要比当前节点小
+        //右子数范围的最大值是maxVal，最小值是当前节点的值，也就是root的值，因为右子树的值要比当前节点大
+        return isValidBST(root.left, root.val, minVal) && isValidBST(root.right,maxVal,root.val);
     }
 }
 // @lc code=end
