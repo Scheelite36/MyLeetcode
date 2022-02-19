c3e75ab34227c9730a6f1a267ecc2e9344b95539
scheelite <jwei.gan@qq.com>
2022-02-19 21:12:47 +0800

    mergeTwoLink

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -17,23 +17,40 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode();
-        ListNode temp = head;
-        while(list1 != null && list2 != null){
-            if( list1.val <= list2.val){
-                temp.next = new ListNode(list1.val);
-                list1 = list1.next;
-            }else{
-                temp.next = new ListNode(list2.val);
-                list2 = list2.next;
-            }
-        }
-        if(list1 == null){
-            temp.next = list2;
+
+        // 双指针
+        // ListNode head = new ListNode();
+        // ListNode temp = head;
+        // while(list1 != null && list2 != null){
+        //     if( list1.val <= list2.val){
+        //         temp.next = list1;
+        //         list1 = list1.next;
+        //     }else{
+        //         temp.next = list2;
+        //         list2 = list2.next;
+        //     }
+        //     temp = temp.next;
+        // }
+        // if(list1 == null){
+        //     temp.next = list2;
+        // }else{
+        //     temp.next = list1;
+        // }
+        // return head.next;
+
+        // 递归
+        if(list1==null){
+            return list2;
+        }else if(list2 == null){
+            return list1;
+        }else if(list1.val<list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }else{
-            temp.next = list1;
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-        return head.next;
+
     }
 }
 // @lc code=end
