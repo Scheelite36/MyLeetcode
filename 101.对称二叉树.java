75d3d381235b2d32c2935078d1302fb0e76ac4c1
scheelite <jwei.gan@qq.com>
2022-03-13 23:31:54 +0800

    isSymmetric

---
+++ 101.对称二叉树.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=java
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return isSymmetricHelper(root.left,root.right);
+    }
+
+    public boolean isSymmetricHelper(TreeNode left, TreeNode right){
+        // 相等返回true
+        if (left==null && right==null){
+            return true;
+            // 不同
+        }else if(left == null || right ==null || left.val != right.val){
+            return false;
+        }
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+    }
+}
+// @lc code=end
+
