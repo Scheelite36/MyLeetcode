bfd67bf965535378c0456fb8620eabfccb8cfec0
yb <b.yang@synovate.tech>
2022-03-23 17:48:10 +0800

    script

--- 102.二叉树的层序遍历.java
+++ 102.二叉树的层序遍历.java
@@ -28,6 +28,7 @@ import javax.swing.tree.TreeNode;
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
+        // 如果是空节点直接返回
         if(root==null){
             return res;
         }
@@ -35,18 +36,26 @@ class Solution {
         nodeList.add(root);
         return getTreeNode(nodeList,res);
     }
-
+    /**
+     * 输入一层的节点， 保存该层节点的值 递归
+     * @param treeNodeList
+     * @param res
+     * @return
+     */
     public List<List<Integer>> getTreeNode(List<TreeNode> treeNodeList, List<List<Integer>> res){
+        // 子节点为空直接返回
         if (treeNodeList.isEmpty()){
             return res;
         }
         List<Integer> vals = new ArrayList<>();
         List<TreeNode> childNodes = new ArrayList<>();
         for(TreeNode node:treeNodeList){
+            // 当前节点为空 遍历下一个节点
             if(node==null) {
                 continue;
             }
             vals.add(node.val);
+            // 保存子节点
             if(node.left != null){
                 childNodes.add(node.left);
             }
