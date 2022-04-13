52c2172651c7b098d70d77de38ad25c3b15a01b7
scheelite <jwei.gan@qq.com>
2022-04-14 00:54:00 +0800

    rob

--- 53.最大子序和.java
+++ 53.最大子序和.java
@@ -7,20 +7,20 @@
 // @lc code=start
 class Solution {
     public int maxSubArray(int[] nums) {
-        int len = nums.length;
-        if (len == 1){
+        if (nums.length == 1){
             return nums[0];
         }
         //initialize a dp array,dp[i] represent the max sub array include i
-        int[] dp = new int[len+1];
+        int[] dp = new int[3];
         // save the max array
-        dp[len] = nums[0];
+        dp[2] = nums[0];
         dp[0] = nums[0];
-        for(int i=1;i<len;i++){
-            dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
-            dp[len] = Math.max(dp[i], dp[len]);
+        for(int i=1;i<nums.length;i++){
+            dp[1] = Math.max(dp[0]+nums[i], nums[i]);
+            dp[2] = Math.max(dp[1], dp[2]);
+            dp[0] = dp[1];
         }
-        return dp[len];
+        return dp[2];
     }
 }
 // @lc code=end
