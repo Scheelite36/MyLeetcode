bc0ed74e4ae383f3f1f75ece58970b5e765b622b
yb <b.yang@synovate.tech>
2022-04-28 18:31:14 +0800

    wrong ans

--- Test.java
+++ Test.java
@@ -1,8 +1,10 @@
-
+import java.util.regex.Pattern;
 
 public class Test {
     public static void main(String[] args) {
-        String s="aAa";
-        System.out.println(Double.MAX_VALUE);
+        String pattern = "((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}";
+        System.out.println(Pattern.matches(pattern, "192.168.2.202"));
+        System.out.println(Pattern.matches(pattern, "192.168.2.109"));
+        System.out.println(Pattern.matches(pattern, "192.168.2.1"));
     }
 }
