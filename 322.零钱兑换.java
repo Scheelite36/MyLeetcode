eb02a021e49fd4e6be21ae6c98a6bd3746403afd
yb <b.yang@synovate.tech>
2022-07-18 20:37:05 +0800

    nothing

---
+++ 322.零钱兑换.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=java
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+    }
+}
+// @lc code=end
+
--- stack/四则运算.java
+++ stack/四则运算.java
@@ -3,6 +3,8 @@ package stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * 描述:四则运算
@@ -10,78 +12,41 @@ import java.io.InputStreamReader;
  * 3+2*{1+2*[-4/(8-6)+7]}
  * 输出：
  * 25
+ * 
+ * 解法：
+ * 1. 用两个栈分别压数字和运算符；
+ * 2. 如果当前运算符优先级*\/高于栈顶运算符+-优先级，则将运算符入栈；反之，从数字栈中弹出两个数，从运算符栈中弹出栈顶运算符，进行运算，
+ *    数字栈压入运算结果，符号栈压入当前运算符。重复该操作直到不满足条件。出现左括号，则直接压入；
+ * 3. 出现右括号，则从数字栈中弹出两个数，从运算符栈中弹出栈顶运算符，进行运算，数字栈压入运算结果，重复该操作直到栈顶弹出右括号位置。
  *
  * @date 2022-07-12
  */
 class Main4 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-        String input = "";
-        while ((input = bfr.readLine()) != null) {
-            input = input.replace("[", "(");
-            input = input.replace("{", "(");
-            input = input.replace("}", ")");
-            input = input.replace("]", ")");
-            input = input.replace(")(", ")*(");// 考虑到(xxx)(xxx)情况,中间加上*
-            int result = cal(input);
-            System.out.println(result);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        s.replace("[", "(");
+        s.replace("{", "(");
+        s.replace("}", ")");
+        s.replace("]", ")");
+        s.replace(")(", ")*(");
+        String op1 = "*/";
+        String op2 = "+-";
+        Deque<Integer> numStack = new LinkedList<>();
+        Deque<Character> charStack = new LinkedList<>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if (Character.isDigit(c)){
+                // 读取所有数字
+                int num = c - '0';
+                while(i+1<s.length() && Character.isDigit(s.charAt(i+1))){
+                    i++;
+                    num = num*10 + (s.charAt(i)-'0');
+                }
+                numStack.push(num);
+            } else if (c)
         }
     }
 
-    /*
-     * 四则运算计算
-     * 示例: 3+(9-7)-5*2+6
-     * 本方法处理说明:
-     * 1. (9-7)会被拆出来处理为2, 实际算式为3+2-5*2+6
-     * 2. 该算式可以看做是四个分段的和:3, 2, -5*2, 6 (这些分段是以加减号分割的)
-     * 3. 从左到右处理,当处理到减号时,减号之前的符号是加号,加号可分段(分段即表示两段结果互不影响),那么左边的3可以合并到result; +2同理
-     * 4. 当处理到最后一个加号时,加号之前的符号是*号,该符号不分段,不用将-5合并到result,temp=temp*number=-5*2=-10;
-     * 5. 处理到最后一位6时,加号,分段,将-10合并到result,最后一位6单独分一段;
-     * 6. 将最后一个分段的结果合并到result,最终结果1.
-     */
-    private static int cal(String str) {
-        if (str.length() == 0)
-            return 0;
-        char[] cs = str.toCharArray();
-        char sign = '+';// sign表示上一个运算符,默认为+,表示0+
-        int result = 0;// 结果值,将每一段的结果合并(以加减分隔段)
-        int temp = 0;// 临时中间值,sign运算符左边的值
-        int number = 0;// sign运算符右边的值
-        for (int i = 0; i < cs.length; i++) {
-            char c = cs[i];
-            if (Character.isDigit(c)) {// 字符串转数值
-                number = number * 10 + c - '0';
-            } else if (c == '(') {// 去括号(将括号中的内容转成数值)
-                int count = 1;
-                int j = i;
-                while (count > 0) {
-                    j++;
-                    if (cs[j] == ')')
-                        count--;
-                    if (cs[j] == '(')
-                        count++;
-                }
-                number = cal(str.substring(i + 1, j));// 将括号中的内容截取,j位置是一个")"
-                i = j;
-            }
-            // 每接收到一个新的运算符,都要处理sign运算符,最后一位时也要处理
-            if (c == '+' || c == '-' || c == '*' || c == '/' || i == cs.length - 1) {
-                if (sign == '+') {// 前一个运算符为加号,那么sign运算符左边的数值可以直接合并到result
-                    result += temp;
-                    temp = number;// temp相当于栈顶,将number压入栈顶
-                } else if (sign == '-') {// 减号同加号
-                    result += temp;
-                    temp = -1 * number;
-                } else if (sign == '*') {// 前一个运算符为乘号,那么sign运算符左边的数值需要直接乘右边的值,还不能合并到result
-                    temp *= number;
-                } else if (sign == '/') {
-                    temp /= number;
-                }
-                number = 0;// number置空,等待接收新值
-                sign = c;// 将新符号更新到sign
-            }
-        }
-        result += temp;// 将sign运算符右边的值也合并到result
-        return result;
-    }
+
 }
